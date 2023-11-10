eb76e40334571ebe58c1f496290120b4d2d9efc9
Nestor Otondo <nestor.otondo@gmail.com>
2023-11-10 16:39:11 -0400

    Adding messages to join us

--- chamber/scripts/validate.js
+++ chamber/scripts/validate.js
@@ -1,9 +1,23 @@
-const validPosition = /^[a-zA-Z- ]{7,}$/g;
+const validPosition = /^[a-zA-Z- ]{7,}$/;
 const position = document.getElementById('position');
+const message = document.querySelector('.message');
+const timestamp = document.getElementById('timestamp');
+const submit = document.getElementById('submit');
 
 position.addEventListener('keyup', (event) => {
   if (validPosition.exec(event.target.value) === null)
+  {
     event.target.classList = 'red';
+    message.style.display = 'block';
+  }
   else
+  {
     event.target.classList = 'green';
+    message.style.display = 'none';
+  }
 });
+
+submit.addEventListener('click', () => {
+  date = new Date().now;
+  timestamp.value = date;
+})
